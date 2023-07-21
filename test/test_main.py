b5133556f3012dee27027f2a96d5071491eb1609
Xavier Jeanson <xavier_jeanson@hotmail.ca>
2023-07-21 10:05:59 -0400

    adjust test

--- test/test_main.py
+++ test/test_main.py
@@ -10,7 +10,6 @@ def set_varaible_env():
     os.environ["TICKETS"] = "test"
     os.environ["T_MAX"] = "test"
     os.environ["T_MIN"] = "test"
-    os.environ["DATABASE"] = "test"
     assert True
 
 def test_variable_env():
@@ -19,7 +18,6 @@ def test_variable_env():
     assert "test" == os.environ.get("TICKETS")
     assert "test" == os.environ.get("T_MAX")
     assert "test" == os.environ.get("T_MIN")
-    assert "test" == os.environ.get("DATABASE")
 
 def test_variables_defaut():
     del os.environ["HOST"]
@@ -27,11 +25,9 @@ def test_variables_defaut():
     del os.environ["TICKETS"]
     del os.environ["T_MAX"]
     del os.environ["T_MIN"]
-    del os.environ["DATABASE"]
     main = Main()
     assert main.HOST == "http://34.95.34.5"
     assert main.TOKEN == "Default"
     assert main.TICKETS == 10
     assert main.T_MAX == 90 
     assert main.T_MIN == 10
-    assert main.DATABASE == "Default"
